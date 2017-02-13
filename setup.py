f2f264385057a4fdecaeb52b7b6fda137f697739
Low Kian Seong <lowks@users.noreply.github.com>
2017-02-13 23:10:16 +0800

    Update setup.py
    typo. `description` not `descriptions`.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='routezeug',
     version='0.1.1',
     url='https://github.com/trevorc/routezeug',
-    descriptions='Minimally useful werkzeug routing',
+    description='Minimally useful werkzeug routing',
     author='Trevor Caira',
     author_email='trevor@caira.com',
     py_modules=['routezeug'],
